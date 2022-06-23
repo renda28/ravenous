e34ffdbe673ecc4e5f1fb78e59cecbbd93a8f6c9
renda28 <renzel.david@gmail.com>
2022-06-23 12:54:29 +0100

    move business object to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
 import "../BusinessList/BusinessList";
@@ -6,12 +5,35 @@ import "../SearchBar/SearchBar";
 import SearchBar from "../SearchBar/SearchBar";
 import BusinessList from "../BusinessList/BusinessList";
 
+const business = {
+  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+];
+
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
